dff74b738e5fd9356e7574d58ba36b9d425753cf
Steve <Steve@DESKTOP-CNEJFDN>
2024-11-12 10:12:58 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Assignment_2_1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                                 // Animals
+                                 // Vertabrate
+                    //LAND          //SEA            //AIR
+        }           //Dog           //Whale           //Cardinal
+    }               //Cat           //Dolphin         //Pelican
+}                   //Horse         //Tuna            //Eagle
